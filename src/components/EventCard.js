7aca6a24b98176bcbe8f46e40060a6f45fa917a2
kjhave <kvk5.19072005@gmail.com>
2024-11-09 01:49:51 +0700

    update EventCard, pass date front Event.js

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-function EventCard( { title, bgImg, time } ){
+function EventCard( { event } ){
     return (
-        <div className="inline-block w-64 h-80 shadow-xl bg-white">
+        <div className="inline-block w-64 h-80 shadow-md shadow-slate-400 bg-stone-400 hover:cursor-pointer">
             <div className="h-56 p-2">
-                <div>{title}</div>
-                <img src={bgImg} alt={title} className="brightness-75 contrast-125" />
+                <div>{event.title}</div>
+                <img src={event.imgUrl} alt={event.title} className="brightness-75 contrast-125" />
             </div>
             <div className="h-24 p-2">
-
+                <div>{event.time}</div>
+                <div>{event.date}</div>
             </div>
         </div>
     );
--- src/components/EventSection.js
+++ src/components/EventSection.js
@@ -3,22 +3,13 @@ import EventCard from "./EventCard";
 
 function EventSection( { title, evenList } ){
     return (
-        <div className="flex justify-center flex-wrap gap-4 w-[1200px] px-4 pb-12 shadow-xl bg-[#242527]">
+        <div className="flex justify-center flex-wrap w-[1200px] px-4 pb-12 shadow-xl bg-[#242527]">
             <div className="w-full py-8">
                 <h1 className="text-center h-12 w-3/4 p-2 ml-auto mr-auto text-white uppercase border-b-2 border-blue-50">{title}</h1>
             </div>
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
+            <div className="flex flex-wrap gap-4 w-[1072px]">
+                {evenList===null?<></>:evenList.map( e => <EventCard event={e} />)}
+            </div>
         </div>
     );
 }
