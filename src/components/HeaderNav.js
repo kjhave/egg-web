e779ec3b87ab45fa0fe7436c2c6dfa8f7283b0e1
brianhuster <phambinhanctb2004@gmail.com>
2024-11-14 16:57:35 +0700

    update HeaderNav

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function HeaderNav({ title, url }) {
+// HeaderNav component
+// @param {string} title - The title of the navigation
+// @param {string} url - The URL of the navigation. Provide `#` if you don't want to redirect
+// @param {function|null} callback - The callback function when the navigation is clicked
+function HeaderNav({ title, url, callback }) {
+	if (callback && typeof (callback) === 'function') {
+		return (
+			<Link to={url} className="text-[#b1b2b6] uppercase hover:cursor-pointer h-12 leading-[48px] px-8" onClick={callback}>{title}</Link>
+		)
+	}
 	return (
 		<Link to={url} className="text-[#b1b2b6] uppercase hover:cursor-pointer h-12 leading-[48px] px-8">{title}</Link>
 	)
