9e7193e9369d7f5a39bd39d585a77656a3d01a44
kjhave <kvk5.19072005@gmail.com>
2024-11-15 13:39:39 +0700

    Event_Done

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,12 +2,13 @@ import React from "react";
 
 function EventCard( { event } ){
     return (
-        <div className="inline-block w-64 h-80 shadow-md rounded-lg shadow-slate-400 bg-stone-400 hover:cursor-pointer">
-            <div className="h-56 p-2">
-                <div>{event.title}</div>
-                {event.img}
+        <div className="inline-block w-64 h-80 shadow-lg rounded-lg shadow-slate-400 bg-stone-400 hover:cursor-pointer hover:brightness-150">
+            <div className="h-60 p-2 overflow-hidden relative">
+                <div className="flex justify-center py-4 text-xl rounded-lg bg-white opacity-80 relative z-10">{event.title}</div>
+                <div className="left-0 top-0 w-full rounded-lg overflow-hidden absolute">{event.img}</div>
             </div>
-            <div className="h-24 p-2">
+
+            <div className="flex flex-col justify-center h-20 px-6 py-2 rounded-bl-lg rounded-br-lg">
                 <div>{event.time}</div>
                 <div>{event.date}</div>
             </div>
--- src/components/Events.js
+++ src/components/Events.js
@@ -5,59 +5,59 @@ import __imgUrl from '../assets/eggIcon.jpg';
 const EventsList = [
     {
         id: 0,
-        title:'Deadline Contest Web 1',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 1',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     },
     {
         id: 1,
-        title:'Deadline Contest Web 2 ',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 2 ',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     },
     {
         id: 2,
-        title:'Deadline Contest Web 3 ',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 3 ',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     },
     {
         id: 3,
-        title:'Deadline Contest Web 4',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 4',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     },
     {
         id: 4,
-        title:'Deadline Contest Web 5',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 5',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     },
     {
         id: 5,
-        title:'Deadline Contest Web 6',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 6',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     },
     {
         id: 6,
-        title:'Deadline Contest Web 7',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 7',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     },
     {
         id: 7,
-        title:'Deadline Contest Web 8',
-        img: <img src={__imgUrl} alt={this.title} />,
-        time:'00:00',
-        date:'16/11/2024'
+        title: 'Deadline Contest Web 8',
+        img: <img src={__imgUrl} alt='' />,
+        time: '00:00',
+        date: '16/11/2024'
     }
 ];
 
